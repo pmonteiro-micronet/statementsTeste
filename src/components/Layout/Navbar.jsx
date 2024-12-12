e126e1c92d3085f0883604ea62eae28608d81a0d
Pedro Monteiro <pmonteiro.micronet@gmail.com>
2024-12-12 11:50:07 +0000

    update de idle timer na sidebar e navbar

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -6,16 +6,18 @@ import { FaUser, FaSignOutAlt } from "react-icons/fa";
 import { signOut, useSession } from "next-auth/react";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
-// Função utilitária para substituir underscores por espaços
 function replaceUnderscores(text) {
   return text.replace(/_/g, " ");
 }
 
 export default function NavBar({ listItems, hotels = [], selectedHotelID, setSelectedHotelID }) {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [openMenus, setOpenMenus] = useState(new Set()); // Estado para controlar menus abertos
+  const [openMenus, setOpenMenus] = useState(new Set());
+  const [warningVisible, setWarningVisible] = useState(false);
+  const [countdown, setCountdown] = useState(60);
   const { data: session } = useSession();
   let inactivityTimeout;
+  let warningTimeout;
 
   const toggleMenu = () => {
     setMenuOpen((prev) => !prev);
@@ -25,9 +27,9 @@ export default function NavBar({ listItems, hotels = [], selectedHotelID, setSel
     setOpenMenus((prev) => {
       const newSet = new Set(prev);
       if (newSet.has(key)) {
-        newSet.delete(key); // Fecha o submenu se já estiver aberto
+        newSet.delete(key);
       } else {
-        newSet.add(key); // Abre o submenu
+        newSet.add(key);
       }
       return newSet;
     });
@@ -42,22 +44,61 @@ export default function NavBar({ listItems, hotels = [], selectedHotelID, setSel
     localStorage.setItem("selectedHotelID", hotelID);
   };
 
+  const stopActivityListeners = () => {
+    window.removeEventListener("mousemove", resetInactivityTimer);
+    window.removeEventListener("keydown", resetInactivityTimer);
+  };
+
+  const startActivityListeners = () => {
+    window.addEventListener("mousemove", resetInactivityTimer);
+    window.addEventListener("keydown", resetInactivityTimer);
+  };
+
   const resetInactivityTimer = () => {
+    if (warningVisible) return; // Não reiniciar se o aviso estiver visível
+
     clearTimeout(inactivityTimeout);
+    clearTimeout(warningTimeout);
+    setWarningVisible(false);
+    setCountdown(60);
+
+    warningTimeout = setTimeout(() => {
+      setWarningVisible(true);
+      stopActivityListeners(); // Parar detectores de atividade
+      startCountdown();
+    }, 14 * 60 * 1000);
+
     inactivityTimeout = setTimeout(() => {
       handleLogout();
     }, 15 * 60 * 1000);
   };
 
+  const startCountdown = () => {
+    let timeLeft = 60;
+    const interval = setInterval(() => {
+      timeLeft -= 1;
+      setCountdown(timeLeft);
+      if (timeLeft <= 0) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  };
+
+  const handleContinueSession = () => {
+    setWarningVisible(false);
+    setCountdown(60);
+    startActivityListeners(); // Reiniciar detectores de atividade
+    resetInactivityTimer();
+  };
+
   useEffect(() => {
-    window.addEventListener("mousemove", resetInactivityTimer);
-    window.addEventListener("keydown", resetInactivityTimer);
+    startActivityListeners();
     resetInactivityTimer();
 
     return () => {
-      window.removeEventListener("mousemove", resetInactivityTimer);
-      window.removeEventListener("keydown", resetInactivityTimer);
+      stopActivityListeners();
       clearTimeout(inactivityTimeout);
+      clearTimeout(warningTimeout);
     };
   }, []);
 
@@ -74,12 +115,10 @@ export default function NavBar({ listItems, hotels = [], selectedHotelID, setSel
       {/* Fullscreen Menu */}
       {menuOpen && (
         <div className="fixed inset-0 z-50 bg-white flex flex-col p-6 overflow-auto">
-          {/* Close Button */}
           <button onClick={toggleMenu} className="self-end text-2xl mb-4 text-gray-600">
             &times;
           </button>
 
-          {/* Hotel Selection (Autocomplete inside the menu) */}
           <div className="mb-4">
             <select
               value={selectedHotelID}
@@ -87,7 +126,6 @@ export default function NavBar({ listItems, hotels = [], selectedHotelID, setSel
               className="border p-2 rounded w-full"
             >
               <option value="">Select a hotel</option>
-              {/* Safe mapping over hotels */}
               {Array.isArray(hotels) &&
                 hotels.map((hotel) => (
                   <option key={hotel.propertyID} value={hotel.propertyID}>
@@ -97,7 +135,6 @@ export default function NavBar({ listItems, hotels = [], selectedHotelID, setSel
             </select>
           </div>
 
-          {/* Render Menu Items */}
           <ul>
             {Object.entries(listItems).map(([key, section]) => (
               <li key={key} className="mb-4">
@@ -105,7 +142,7 @@ export default function NavBar({ listItems, hotels = [], selectedHotelID, setSel
                   className="flex items-center justify-between cursor-pointer font-bold text-indigo-800 mb-2"
                   onClick={() => toggleSubmenu(key)}
                 >
-                  {replaceUnderscores(key)} {/* Aplica a função aqui */}
+                  {replaceUnderscores(key)}
                   {section.items && (
                     <span className="text-sm text-gray-600">
                       {openMenus.has(key) ? <IoIosArrowUp size={15} /> : <IoIosArrowDown size={15} />}
@@ -113,7 +150,6 @@ export default function NavBar({ listItems, hotels = [], selectedHotelID, setSel
                   )}
                 </div>
 
-                {/* Submenu */}
                 {openMenus.has(key) && (
                   <ul className="pl-4">
                     {section.items.map((item, index) =>
@@ -128,7 +164,7 @@ export default function NavBar({ listItems, hotels = [], selectedHotelID, setSel
                                   className="block py-1 text-indigo-600"
                                   onClick={toggleMenu}
                                 >
-                                  {replaceUnderscores(subItem.label)} {/* Aplica a função aqui */}
+                                  {replaceUnderscores(subItem.label)}
                                 </Link>
                               </li>
                             ))}
@@ -141,7 +177,7 @@ export default function NavBar({ listItems, hotels = [], selectedHotelID, setSel
                             className="block py-2 text-indigo-600"
                             onClick={toggleMenu}
                           >
-                            {replaceUnderscores(item.label)} {/* Aplica a função aqui */}
+                            {replaceUnderscores(item.label)}
                           </Link>
                         </li>
                       )
@@ -152,9 +188,7 @@ export default function NavBar({ listItems, hotels = [], selectedHotelID, setSel
             ))}
           </ul>
 
-          {/* Profile and Logout at the bottom */}
           <div className="mt-auto flex flex-col">
-            {/* Profile */}
             <div className="flex items-center mb-4">
               <div className="w-10 h-10 rounded-full bg-primary-50 flex items-center justify-center text-primary">
                 <FaUser />
@@ -168,8 +202,6 @@ export default function NavBar({ listItems, hotels = [], selectedHotelID, setSel
                 </span>
               </div>
             </div>
-
-            {/* Logout Button */}
             <button
               onClick={handleLogout}
               className="flex items-center justify-center text-white bg-red-500 h-10 rounded-lg hover:bg-red-600"
@@ -177,6 +209,29 @@ export default function NavBar({ listItems, hotels = [], selectedHotelID, setSel
               <FaSignOutAlt className="mr-2" />
               Logout
             </button>
+          </div>
+        </div>
+      )}
+
+      {/* Modal Warning */}
+      {warningVisible && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
+          <div className="bg-white p-6 rounded shadow-md w-80 text-center">
+            <h1 className="text-xl font-bold mb-4">You will logout automatically in {countdown} seconds!</h1>
+            <div className="flex justify-around mt-4">
+            <button
+                onClick={handleContinueSession}
+                className="bg-primary text-white px-4 py-2 rounded hover:bg-green-600"
+              >
+                Continuar Sessão
+              </button>
+              <button
+                onClick={handleLogout}
+                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+              >
+                Logout
+              </button>
+            </div>
           </div>
         </div>
       )}
