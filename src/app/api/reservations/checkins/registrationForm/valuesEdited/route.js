dc664ea206d94b078de168e7f1f8284f6019b4b6
Simone Santos <116946584+enomis125@users.noreply.github.com>
2024-12-20 18:44:27 +0000

    fix

--- src/app/api/reservations/checkins/registrationForm/valuesEdited/route.js
+++ src/app/api/reservations/checkins/registrationForm/valuesEdited/route.js
@@ -4,11 +4,14 @@ import prisma from "@/lib/db";
 
 export async function POST(request) {
   try {
-    // Obtém os parâmetros da URL
-    const { searchParams } = new URL(request.url);
-    const PropertyID = searchParams.get("propertyID");
+    // Obtém os dados do corpo da requisição
+    const body = await request.json();
+    console.log("Dados recebidos no backend:", body);
 
-    // Verifica se os parâmetros estão presentes
+    // Acessa o parâmetro propertyID do corpo da requisição
+    const PropertyID = body.propertyID;
+
+    // Verifica se o parâmetro PropertyID está presente
     if (!PropertyID) {
       return new NextResponse(
         JSON.stringify({ error: "Faltam parâmetros: PropertyID" }),
@@ -41,17 +44,13 @@ export async function POST(request) {
 
     const { propertyServer, propertyPort } = property;
 
-    // Obtém os dados do corpo da requisição
-    const body = await request.json();
-    console.log("Dados recebidos no backend:", body);
-
-    // Envia valores vazios se não houver alteração
+    // Prepara os dados para enviar ao servidor externo
     const dataToSend = {
-      email: body.email || "",
-      vatNo: body.vatNo || ""
+      email: body.email !== undefined ? body.email : null,
+      vatNo: body.vatNo !== undefined ? body.vatNo : null
     };
 
-    // Verifica se os campos necessários estão presentes
+    // Verifica se pelo menos um dos campos (email ou vatNo) está presente
     if (!dataToSend.email && !dataToSend.vatNo) {
       return new NextResponse(
         JSON.stringify({ error: "Faltam dados: email ou vatNo" }),
@@ -63,17 +62,17 @@ export async function POST(request) {
 
     // Define a URL de destino dinamicamente
     const url = `http://${propertyServer}:${propertyPort}/valuesEdited`;
+    console.log("URL de destino:", url);
 
     // Envia os dados para o servidor externo
     const response = await axios.post(url, dataToSend);
-
     console.log("Resposta da API externa:", response.data);
 
     return new NextResponse(
       JSON.stringify({ message: "Dados recebidos e enviados com sucesso", data: response.data }),
       { status: 200, headers: { 'Content-Type': 'application/json; charset=utf-8' } }
     );
-    
+
   } catch (error) {
     console.error("Erro ao processar os dados:", error.response ? error.response.data : error.message);
     return new NextResponse(
