dec5612e00270411db51e91e9ad5eaf959e43580
Nene ♡ <scnene23@gmail.com>
2025-02-18 11:14:49 +0000

    Update route.js

--- src/app/api/reservations/checkins/registrationForm/countries/route.js
+++ src/app/api/reservations/checkins/registrationForm/countries/route.js
@@ -42,8 +42,14 @@ export async function GET(request) {
     const url = `http://${propertyServer}:${propertyPort}/nationalities`;
 
     // Fazer a requisição para buscar as nacionalidades
-    const response = await axios.get(url);
-    console.log(response)
+    const response = await axios.get(url, {
+    headers: {
+      Authorization: 'q4vf9p8n4907895f7m8d24m75c2q947m2398c574q9586c490q756c98q4m705imtugcfecvrhym04capwz3e2ewqaefwegfiuoamv4ros2nuyp0sjc3iutow924bn5ry943utrjmi',
+      'Content-Type': 'application/json',
+    },
+  });
+    console.log(response);
+
     const nationalities = response.data;
 
     // Retornar os dados como JSON
