a0a9454f0cec781a6ab734d0b3864fdfdfd59645
Pedro Monteiro <pmonteiro.micronet@gmail.com>
2025-10-06 17:48:39 +0100

    alteraçao de logica de politica privacidade

--- src/app/homepage/frontOfficeView/registrationForm/page.js
+++ src/app/homepage/frontOfficeView/registrationForm/page.js
@@ -568,7 +568,7 @@ export default function Page() {
                 {
                     PropertyID: propertyID,
                     pdfBase64: pdfBase64, // Envia o PDF comprimido em Base64
-                    fileName: `RegistrationForm_ResNo_${reserva.ResNo}_TC_${termsAccepted ? 0 : 1}_DPP_${policyAccepted ? 0 : 1}_ProfileID_${guestInfo.ProfileID}.pdf`,
+                    fileName: `RegistrationForm_ResNo_${reserva.ResNo}_TC_${termsAccepted ? 1 : 0}_DPP_${policyAccepted ? 1 : 0}_ProfileID_${guestInfo.ProfileID}.pdf`,
                 }
             );
 
