948541c8e96d589034ffc35bf312461cf4b5506a
ssantos.micronet <ssantos.micronet@gmail.com>
2025-10-10 17:03:41 +0100

    jsons in real time

--- src/components/modals/arrivals/reservationForm/PersonalID/page.jsx
+++ src/components/modals/arrivals/reservationForm/PersonalID/page.jsx
@@ -210,7 +210,7 @@ const PersonalIDForm = ({ onClose, onSave, personalID, propertyID, t }) => {
                                 <div className="flex flex-row justify-between w-full gap-4">
                                     <div className="w-1/3">
                                         <label className="block text-sm font-medium">
-                                            {t.modals.PersonalID.dateofBirth}
+                                            {t.modals.PersonalID.dateofBirth} *
                                         </label>
 
                                         <div className="flex items-center gap-2">
@@ -238,7 +238,7 @@ const PersonalIDForm = ({ onClose, onSave, personalID, propertyID, t }) => {
 
                                                     // ⚠️ Verifica se é uma data futura
                                                     if (value.length === 10 && !isNaN(inputDate.getTime()) && inputDate > today) {
-                                                        setDobError("A data de nascimento não pode ser superior à data de hoje.");
+                                                        setDobError(t.modals.errors.invalidDate);
                                                     } else {
                                                         setDobError("");
                                                     }
@@ -249,8 +249,8 @@ const PersonalIDForm = ({ onClose, onSave, personalID, propertyID, t }) => {
                                                 placeholder="aaaa/mm/dd"
                                                 maxLength={10}
                                                 className={`w-full border rounded-md px-2 py-2 
-        focus:outline focus:outline-black focus:ring-2 focus:ring-black 
-        ${dobError ? "border-red-500" : "border-gray-300"}`}
+                                                focus:outline focus:outline-black focus:ring-2 focus:ring-black 
+                                                ${dobError ? "border-red-500" : "border-gray-300"}`}
                                             />
 
                                             {/* Ícone do calendário */}
